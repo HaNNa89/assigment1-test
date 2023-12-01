f28f21c19a3f784f1a2e67b8e5e4fa3768014652
HaNNa89 <petersson.h@hotmail.com>
2023-12-01 13:30:39 +0100

    added a new component for result

---
+++ src/components/ResultDataList.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+function ResultDataList({ meanings }) {
+	if (!meanings) {
+		return null;
+	}
+	console.log(meanings[0]);
+	if (meanings[0].phonetics) {
+		console.log(meanings[0].phonetics.find((phonetic) => phonetic.audio));
+	}
+
+	return (
+		<div>
+			{meanings.map((val) => {
+				console.log(val);
+				console.log(val.phonetics.find((phonetic) => phonetic.audio).audio);
+				return val.meanings.map((meanings, index) => (
+					<div key={index}>
+						<h3>{meanings.partOfSpeech}</h3>
+						<ul>
+							{meanings.definitions.map((def) => (
+								<li key={def.definition}>{def.definition}</li>
+							))}
+						</ul>
+						<div key={index}>
+							<h1>asdasd</h1>
+							{val.phonetics && (
+								<audio
+									controls
+									src={val.phonetics.find((phonetic) => phonetic.audio).audio}
+									type="audio/mpeg"
+								></audio>
+							)}
+						</div>
+
+						{meanings.synonyms && (
+							<>
+								<h3>Synonyms:</h3>
+								<ul>
+									{meanings.synonyms.map((syn, index) => (
+										<li key={index}>{syn}</li>
+									))}
+								</ul>
+							</>
+						)}
+						{meanings.antonyms && (
+							<>
+								<h3>Antonyms:</h3>
+								<ul>
+									{meanings.antonyms.map((ant, index) => (
+										<li key={index}>{ant}</li>
+									))}
+								</ul>
+							</>
+						)}
+						{meanings.example && (
+							<>
+								<h3>Example:</h3>
+								<ul>
+									<li key={meanings.example}>{meanings.example}</li>
+								</ul>
+							</>
+						)}
+					</div>
+				));
+			})}
+		</div>
+	);
+}
+
+export default ResultDataList;
