214bbd027a57199445190b3b8a59a105ec3e8cd5
HaNNa89 <petersson.h@hotmail.com>
2023-12-01 13:23:34 +0100

    changed code in SearchBox

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,12 +1,39 @@
-function SearchBox() {
+import { useState } from "react";
+
+function SearchBox({ inputValue, setInputValue }) {
+	const [value, setValue] = useState("");
+
+	const handleInputChange = (e) => setValue(e.target.value);
+
+	const handleSubmit = () => {
+		setInputValue(value);
+		setValue("");
+	};
+
+	const handleInputKeyDown = (e) => {
+		if (e.key === "Enter") {
+			handleSubmit();
+		}
+	};
+
 	return (
-		<>
-			<div>
-				<input type="text" placeholder="search..." />
-				<button>Search</button>
-			</div>
-		</>
+		<div>
+			<h4>Search for a word</h4>
+			<input
+				type="text"
+				placeholder="type your word here..."
+				onChange={handleInputChange}
+				value={value}
+				onKeyDown={handleInputKeyDown}
+			/>
+			<button onClick={handleSubmit}>Search</button>
+
+			{inputValue && (
+				<h2>
+					Result for: <span>{inputValue}</span>
+				</h2>
+			)}
+		</div>
 	);
 }
-
 export default SearchBox;
