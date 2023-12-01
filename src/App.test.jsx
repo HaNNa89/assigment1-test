5b724034f586bfdbfbf1233d009b335c196c60dd
HaNNa89 <petersson.h@hotmail.com>
2023-12-02 00:45:07 +0100

    created a test for my placeholder  - it works now

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import Header from "./components/Header";
+import SearchBox from "./components/SearchBox";
 
 describe("something truthy and falsy", () => {
 	it("true to be true", () => {
@@ -18,4 +19,10 @@ test('should display a header with the text "Dictionary"', () => {
 	render(<Header />);
 	const header = screen.getByText("Dictionary");
 	expect(header).toBeInTheDocument();
+});
+
+test("should find input placeholder text", () => {
+	render(<SearchBox />);
+	const input = screen.getByPlaceholderText("type your word here...");
+	expect(input).toBeInTheDocument();
 });
