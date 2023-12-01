b2795f30371d3ec69c6d2ba978207db7c44c7724
HaNNa89 <petersson.h@hotmail.com>
2023-12-02 00:21:51 +0100

    created a test for Header - it works

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,6 @@
+import { render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
+import Header from "./components/Header";
 
 describe("something truthy and falsy", () => {
 	it("true to be true", () => {
@@ -8,4 +10,12 @@ describe("something truthy and falsy", () => {
 	it("false to be false", () => {
 		expect(false).toBe(false);
 	});
+});
+
+//Syntax
+
+test('should display a header with the text "Dictionary"', () => {
+	render(<Header />);
+	const header = screen.getByText("Dictionary");
+	expect(header).toBeInTheDocument();
 });
