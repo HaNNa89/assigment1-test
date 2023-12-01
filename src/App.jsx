b20cf0675f994f956e209c3c5ae409b0c4b845a3
HaNNa89 <petersson.h@hotmail.com>
2023-12-01 13:19:27 +0100

    added new components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import Audio from "./components/Audio";
+import { useState } from "react";
 import ContentBox from "./components/ContentBox";
+import Header from "./components/Header";
+import ResultDataList from "./components/ResultDataList";
 import SearchBox from "./components/SearchBox";
 
 function App() {
+	const [inputValue, setInputValue] = useState("");
+
 	return (
-		<>
-			<div>
-				<h1>Hej Dictionary</h1>
-				<SearchBox />
-				<ContentBox />
-				<Audio />
-			</div>
-		</>
+		<div>
+			<Header />
+			<SearchBox inputValue={inputValue} setInputValue={setInputValue} />
+			<ContentBox inputValue={inputValue} />
+			<ResultDataList inputValue={inputValue} />
+		</div>
 	);
 }
 
